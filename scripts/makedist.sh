8e1aafb109fa5be86360f47baa864210e32d6c12
Massimiliano Marcon <emaildelmax@gmail.com>
2011-09-05 09:34:15 -0400

    Added distribution (stable) version. Created bash script that compresses jOVI using closure compiler API and generates distribution.

---
+++ scripts/makedist.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Check if curl is present
+which curl > /dev/null
+if [ $? != "0" ]; then
+    echo "curl is missing, compilation cannot be performed"
+    exit 1
+fi
+
+echo -n "Initializing... "
+
+CURRENT_DIR=`pwd`
+DIST_DIR=../dist
+TEMP_DIR=/tmp/jOVI.`date "+%Y%m%d-%H.%M"`
+INFILE=../src/jquery.jovi.js
+OUTFILE_MIN=$TEMP_DIR/jquery.jovi.min.js
+OUTFILE_NORMAL=$TEMP_DIR/jquery.jovi.js
+
+#Make temporary directory
+mkdir $TEMP_DIR
+
+echo "Done."
+
+#Compress with Closure Compiler API
+echo -n "Contacting API and sending file... "
+curl -s -d compilation_level=SIMPLE_OPTIMIZATIONS -d output_info=compiled_code -d output_format=text --data-urlencode "js_code@${INFILE}" http://closure-compiler.appspot.com/compile > $OUTFILE_MIN
+echo "Done."
+#Copy uncompressed version as well
+cp $INFILE $OUTFILE_NORMAL
+#TarGz everything together
+echo -n "Building dist... "
+tar cvzf $DIST_DIR/jOVI.tar.gz --directory $TEMP_DIR/.. `basename ${TEMP_DIR}` &> /dev/null
+#Remove temporary directory
+rm -rf $TEMP_DIR > /dev/null
+echo "Done."
